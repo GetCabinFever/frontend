f77808369535d5d04f70083606c6c771f02cc69e
JONATHAN SILVA <silva.jon86@gmail.com>
2016-04-27 12:52:24 -0400

    started building jsx for generate_new page

--- src/js/generate_new.js
+++ src/js/generate_new.js
@@ -1,13 +1,15 @@
-import React, { Component }  from 'react';
+import React, { Component, PropTypes }  from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory, IndexRoute, Link } from 'react-router';
 import { ajax } from 'jquery';
 
 export default class GenerateNew extends Component {
+
 	render() {
+
 		return (
 			<div>
-			<h1> HI USER, GUESS WHA? </h1>
+			<h1> HEY!!! </h1>
 			<h2> IT WORKS!!! MAYBE? </h2>
 			</div>
 		);
