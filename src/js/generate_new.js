b2e788e302f53945acd31813f097c74009b89821
JONATHAN SILVA <silva.jon86@gmail.com>
2016-04-27 10:30:34 -0400

    Reworked Ameneties section in create new listing

---
+++ src/js/generate_new.js
@@ -0,0 +1,12 @@
+import React, { Component }  from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute, Link } from 'react-router';
+import { ajax } from 'jquery';
+
+export default class GenerateNew extends Component {
+	render() {
+		return (
+			<h1> HI IT WORKS </h1>
+		);
+	}
+}
