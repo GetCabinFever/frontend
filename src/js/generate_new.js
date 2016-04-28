88cefe2e4c75e3888c9411aa50ddfd411b5731dd
JONATHAN SILVA <silva.jon86@gmail.com>
2016-04-28 11:03:19 -0400

    Added renderPage and renderLoading functions in the generate_new file so the page could load properly.

--- src/js/generate_new.js
+++ src/js/generate_new.js
@@ -6,50 +6,45 @@ import { ajax } from 'jquery';
 export default class GenerateNew extends Component {
 	constructor(...props){
 		super(...props);
-		this.state={ cabininfo:{} };
+		this.state={ cabininfo:{},
+			loading: true  
+		};
 	}
-
-	// componentWillMount(data){
-	// 		ajax({
-	// 			url: 'https://cabinfever.heroku.com/residences',
-	// 			type: 'GET',
-	// 			data: data,
-	// 			cache: false,
-	// 			dataType: 'json'
-	// 		}).then(response=>{ 
-	// 			console.log(response) 
-	// 			this.setState({response})
-	// 		})
-	// 	}
 
 	componentWillMount(){
 		let { cabininfo } = this.props.params;
-		console.log(this.props.params)
-		ajax(`https://cabinfever.herokuapp.com/residences/${cabininfo}`).then(data => { 
-			console.log(data);
-			this.setState({cabininfo: data});
+		// console.log(this.props.params)
+		ajax(`https://cabinfever.herokuapp.com/residences/${cabininfo}`).then(cabininfo => { 
+			// console.log(data);
+
+			this.setState({cabininfo, loading: false});
 		})
 	}
 
-	// {CabinInfo[0].id} 
-
-	render() {
+	renderPage(){
 		let { cabininfo } = this.state;
-		console.log(cabininfo);
+
+		console.log('fuck all the things');
+		console.log('cabininfo.residence ===>', cabininfo.residence);
+		// console.log('cabininfo.residence.bathrooms ===>', cabininfo.residence.bathrooms);
 		return (
 			<div>
 				<div className="top">
-					<div className="hero-image"></div>
-					<div className="title"></div>	
-					<div className="property-description"></div>
-					<div className="property-link"></div>	
+					<div className="hero-image">
+						<img src={cabininfo.image_url}/>
+					</div>
+					<div className="title">
+						<h1></h1>
+					</div>	
+					<div className="property-description">
+						<div>{cabininfo.residence.address}</div>
+					</div>
+					<div className="property-link">
+					</div>	
 				</div>
 				
 				<div className="middle">
 					<div className="basic-property-info">
-
-						<h1>All Cabins Info</h1>
-						<img src={cabininfo.image_url}/>
 
 					</div>
 					<div className="prices"></div>
@@ -65,17 +60,15 @@ export default class GenerateNew extends Component {
 			</div>
 		);
 	}
+
+	
+
+renderLoading(){
+	return (<div> Loading... </div>)
 }
 
-// componentWillMount(){
-// 		ajax({
-// 			url: 'https://cabinfever.heroku.com/residences',
-// 			type: 'GET',
-// 			data: data,
-// 			cache: false,
-// 			dataType: 'json'
-// 		}).then(response=>{ 
-// 			console.log(response) 
-// 			this.setState({response})
-// 		})
-// 	}
+	render() {
+		let { loading } = this.state;
+		return loading ? this.renderLoading() : this.renderPage()
+	}
+}
