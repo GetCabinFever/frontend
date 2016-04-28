7c05bf46eff38e9883d5b079bf7eb0c149540e94
Alex Windle <alexwindle85@gmail.com>
2016-04-28 16:20:35 -0400

    add image dropzone to registration.

--- src/js/register-modal.js
+++ src/js/register-modal.js
@@ -8,10 +8,15 @@ import Main from './main';
 import CreateNewListing from './create-new-listing';
 import Dashboard from './dashboard';
 import SimpleSerialForm from 'react-simple-serial-form';
+import Dropzone from 'react-dropzone';
 
 export default class RegisterModal extends Component {
-	
-	// var currentUser = null;
+	constructor(...args){
+		super(...args);
+		this.state = {
+			preview: 'http://ecx.images-amazon.com/images/I/81mQERthQjL._SL1500_.jpg'
+		}
+	}
 
 	dataHandler(data){
 		console.log('data', data);
@@ -40,6 +45,10 @@ export default class RegisterModal extends Component {
 			});
 		};
 
+	dropHandler([file]){
+		this.setState({ preview: file.preview })
+	}
+
 	onBack(){
 		hashHistory.push('/')
 	}
@@ -58,6 +67,11 @@ export default class RegisterModal extends Component {
 					<input type="tel"      name="phone"      placeholder="Phone number"                 />
 					<input type="date"     name="dob"        placeholder=" "                            />
 					<input type="password" name="password"   placeholder="Password"                     />
+
+					<Dropzone onDrop={ ::this.dropHandler } >
+						<img src={ this.state.preview } height='200px' width='200px' />
+						<input type='hidden' name='avatar' value= { this.state.preview } />
+					</Dropzone>
 
 					<button>Register</button>
 				</SimpleSerialForm>
